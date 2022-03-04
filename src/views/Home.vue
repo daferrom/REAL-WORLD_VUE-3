fea8e0989c9d9a0b6541c6c7417f028f7246179d
daferrom <daferrom@gmail.com>
2022-03-04 14:10:40 -0500

    simgle file components lesson 3 completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
+  },
+  data(){
+    return { 
+      events: [
+       {
+        id: 5928101,
+        category: 'animal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend at this event',
+        location: 'Meow Town',
+        date:'January 28,2022',
+        time:'12:00',
+        petsAllowed: true,
+        organizer: 'Kat Laydee'
+      },
+      {
+        id: 5928102,
+        category: 'animal welfare',
+        title: 'Party with your pets',
+        description: 'Have a good time with your friends and pets',
+        location: 'Bogotá',
+        date:'may 4,2022',
+        time:'3:00',
+        petsAllowed: true,
+        organizer: 'Disco stu'
+      },
+      {
+        id: 5928103,
+        category: 'webinar',
+        title: 'Are cats the new gods?',
+        description: 'A discusson about the cateology',
+        location: 'New York',
+        date:'dec 15,2022',
+        time:'4:00',
+        petsAllowed: false,
+        organizer: 'MIT'
+      }
+      ]
+    }
   }
 };
 </script>
+
+<style scoped>
+.events{
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
+
+      
